bd97303740dd413783b6f809246d2da864e4acf1
juantaborda27 <juantabordaacostamail.com>
2025-03-27 11:58:03 -0500

    creacion de updateTarea y deleteTarea

--- lib/data/repositories/tarea_repository.dart
+++ lib/data/repositories/tarea_repository.dart
@@ -56,4 +56,37 @@ class TareaRepository {
       throw Exception('Error de conexion3');
     }
   }
+
+
+  Future<Tarea> updateTarea(int id, Tarea tarea) async{
+    try {
+      final response = await http.put(
+        Uri.parse('$baseUrl/tareas/$id'),
+        headers: {'Content-Type': 'application/json'},
+        body: json.encode(tarea.toJson()),
+      );
+
+      if(response.statusCode == 200){
+        return Tarea.fromJson(json.decode(response.body));
+      }else{
+        throw Exception('Error al actualizar la tarea ${response.statusCode}');
+      }
+    } catch (e) {
+      throw Exception('Error de conexion4: $e');
+    }
+  }
+
+
+  Future<void> deleteTarea(int id) async{
+    try {
+      final response = await http.delete(Uri.parse('$baseUrl/tareas/$id'));
+      if(response.statusCode == 204){
+        return;
+      }else{
+        throw Exception('Error al eliminar la tarea ${response.statusCode}');
+      }
+    } catch (e) {
+      throw Exception('Error de conexion5: $e');
+    }
+  }
 }
